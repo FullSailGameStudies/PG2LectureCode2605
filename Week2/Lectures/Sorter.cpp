0b9fadc641194b2ed4eee2535245014bf30468a9
Garrett Girod <girodg@hotmail.com>
2026-05-11 11:23:41 -0400

    bubble sort

--- Week2/Lectures/Sorter.cpp
+++ Week2/Lectures/Sorter.cpp
@@ -4,3 +4,49 @@
 //
 // Part A-2
 //
+/*
+		procedure bubbleSort(A : list of sortable items)
+		  n := length(A)
+		  repeat
+			  swapped := false
+			  for i := 1 to n - 1 inclusive do
+				  if A[i - 1] > A[i] then
+					  swap(A, i - 1, i)
+					  swapped = true
+				  end if
+			  end for
+			  n := n - 1
+		  while swapped
+		end procedure
+*/
+void Sorter::bubbleSort(std::vector<Light>& A)
+{
+	//n := length(A)
+	int n = A.size();
+
+	//repeat
+	bool swapped = false;
+	do
+	{
+		//swapped: = false
+		swapped = false;
+		//for i := 1 to n - 1 inclusive do
+		for (int i = 1; i <= n - 1; i++)
+		{
+			//if A[i - 1] > A[i] then
+			if (A[i - 1].red > A[i].red)
+			{
+				//swap(A, i - 1, i) //TODO! figure this out
+				//OG Swap...
+				//Light temp = A[i - 1]; //{2,1} temp = 2
+				//A[i - 1] = A[i]; //{1,1}
+				//A[i] = temp;//{1,2}
+				std::swap(A[i - 1], A[i]);
+				//swapped = true
+				swapped = true;
+			}//end if
+		}//end for
+		//n := n - 1
+		--n;
+	} while (swapped);//while swapped
+}//end procedure
